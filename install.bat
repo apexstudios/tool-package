cf6056581ecec1b4f57adeb8209092b8664a59f4
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-15 16:06:51 +0100

    modifying install.bat a little bit

--- install.bat
+++ install.bat
@@ -3,9 +3,41 @@
 goto startmsg
 
 :setenv
+echo.
+echo  Setting environment variables
+echo.
 
+echo SetEnv PATH for PHP to
+echo %CD%\PHP
 SetEnv -a PATH %%%CD%\PHP
-SetEnv -a PATH %%%CD%\Arcanist\bin
+echo Check
+
+echo.
+
+echo SetEnv PATH for Arcanist
+echo %CD%\Arcanist\bin
+SetEnv -a PATH %%%CD%\Arcanist\bin & echo Check  echo Not
+echo Check
+echo.
+echo  Sucessfully set environment variables
+
+goto selectmsg
+
+:install-certificate
+echo.
+echo  I will now install the Arcanist certificate for you.
+echo  This will enable to identify you in Phabricator without you having to type in your username
+echo  or password for Arcanist.
+echo.
+echo  You will be asked to open the Phabricator website, which will require you to login. Just keep
+echo  %PHAB%conduit/token open already, you'll save yourself some work
+echo.
+echo Ready?
+pause
+echo.
+
+arc install-certificate
+goto selectmsg
 
 :startmsg
 echo  This will install the Apex Studios Tool Package
@@ -29,6 +61,13 @@ pause
 set BINDIR=Binaries\
 set PHAB=http://phabricator.burningreality.de/
 
+:selectmsg
+echo.
+echo  1. Set up environment variables
+echo  2. Install certificate for Arcanist
+echo  3. Do some things
+echo  4. Environment variables not set?
+echo     1) Try to restart
 
-echo %CD%PHP
+
 
